8ae4c58b1409c6390cbae175fef6aa318f771634
LeXScripterX <LeXScripterX>
2024-07-17 14:20:27 -0500

    Busg_Corregido_Final

---
+++ toDoList/tasks/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from .models import Task
+
+class TaskForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = ['title', 'description', 'status']
+
+
+class SignupForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'password1', 'password2']
+
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
--- toDoList/tasks/views.py
+++ toDoList/tasks/views.py
@@ -1,3 +1,74 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth import login 
+from django.contrib.auth.forms import AuthenticationForm
+from django.urls import reverse_lazy
+from django.views import View
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Task
+from .forms import TaskForm, SignupForm
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth import views as auth_views
 
-# Create your views here.
+class TaskListView(LoginRequiredMixin, ListView):
+    model = Task
+    template_name = 'tasks/task_list.html'
+    context_object_name = 'tasks'
+
+    def get_queryset(self):
+        if self.request.user.is_superuser:
+            return Task.objects.all()
+        return Task.objects.filter(user=self.request.user)
+
+class TaskDetailView(LoginRequiredMixin, UserPassesTestMixin, DetailView):
+    model = Task
+    template_name = 'tasks/task_detail.html'
+
+    def test_func(self):
+        task = self.get_object()
+        return self.request.user == task.user or self.request.user.is_superuser
+
+class TaskCreateView(LoginRequiredMixin, CreateView):
+    model = Task
+    form_class = TaskForm
+    template_name = 'tasks/task_form.html'
+    success_url = reverse_lazy('task_list')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+class TaskUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Task
+    form_class = TaskForm
+    template_name = 'tasks/task_form.html'
+    success_url = reverse_lazy('task_list')
+
+    def test_func(self):
+        task = self.get_object()
+        return self.request.user == task.user or self.request.user.is_superuser
+
+class TaskDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Task
+    template_name = 'tasks/task_confirm_delete.html'
+    success_url = reverse_lazy('task_list')
+
+    def test_func(self):
+        task = self.get_object()
+        return self.request.user == task.user or self.request.user.is_superuser
+
+class SignupView(CreateView):
+    template_name = 'login/signup.html'
+    form_class = SignupForm
+    success_url = reverse_lazy('task_list')
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect(self.success_url)
+
+class LoginView(auth_views.LoginView):
+    template_name = 'login/login.html'
+
+class IndexView(View):
+    def get(self, request):
+        return render(request, 'index.html')
